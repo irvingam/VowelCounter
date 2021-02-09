fb53ed23a6d30c7ee8045532061ca47923dff274
Andrew-Irving <amirving94@gmail.com>
2021-02-08 21:09:21 -0500

    getVowels function will create a statement when submitted

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // Selectors
-
+const submitBtn = document.getElementById('submit--btn')
+const string = document.getElementById('input--area__string')
+const bottomHTML = document.querySelector('.bottom')
 
 // Event
+submitBtn.addEventListener('click', getVowels)
 
 
 // Functions
 
+function getVowels() {
+    let str = string.value
+    let total = 0
+
+
+
+    const statement = document.createElement('p')
+    statement.innerText = 'This statement has x vowels'
+    statement.classList.add('vowelstatement')
+    bottomHTML.appendChild(statement)
+}
