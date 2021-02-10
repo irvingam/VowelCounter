1bf9cd2fd3a64c536ceb71bbeb54496a05f56a27
Andrew-Irving <amirving94@gmail.com>
2021-02-09 20:45:30 -0500

    added vowel calculating function

--- app.js
+++ app.js
@@ -4,19 +4,43 @@ const string = document.getElementById('input--area__string')
 const bottomHTML = document.querySelector('.bottom')
 
 // Event
-submitBtn.addEventListener('click', getVowels)
+submitBtn.addEventListener('click', () => {
+    // Constant that reads the vowels from the string
+    const countVowels = str => Array.from(str)
+        .filter(letter => 'aeiou'.includes(letter)).length;
+
+    // Calculated total number of vowels within the string
+    const total = countVowels(string.value)
+
+     // Will create a statment showing how many vowels are in the string
+     const statement = document.createElement('p')
+     statement.innerText = `This statement has ${total} vowels`
+     statement.classList.add('vowelstatement')
+     bottomHTML.appendChild(statement)
+
+
+})
 
 
 // Functions
-
-function getVowels() {
-    let str = string.value
+/*
+function getVowels(str) {
+    str = string.value 
     let total = 0
 
+    return str.replace(/[^aeiou]/gi, "").length
 
 
+    // Will create a statment showing how many vowels are in the string
     const statement = document.createElement('p')
     statement.innerText = 'This statement has x vowels'
     statement.classList.add('vowelstatement')
     bottomHTML.appendChild(statement)
+
+    console.log(getVowels(string.value))
 }
+
+console.log(getVowels(string.value)) */
+
+
+
