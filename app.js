72e14a4d7286f431849b2ab6191ed4e08b103c3a
Andrew-Irving <amirving94@gmail.com>
2021-02-09 21:12:20 -0500

    alert functions shows number of vowels

--- app.js
+++ app.js
@@ -2,45 +2,20 @@
 const submitBtn = document.getElementById('submit--btn')
 const string = document.getElementById('input--area__string')
 const bottomHTML = document.querySelector('.bottom')
+const vowelstatement = document.getElementsByTagName('p')
 
 // Event
 submitBtn.addEventListener('click', () => {
     // Constant that reads the vowels from the string
     const countVowels = str => Array.from(str)
-        .filter(letter => 'aeiou'.includes(letter)).length;
+        .filter(letter => 'aeiouAEIOU'.includes(letter)).length;
 
     // Calculated total number of vowels within the string
-    const total = countVowels(string.value)
+    let total = countVowels(string.value)
 
-     // Will create a statment showing how many vowels are in the string
-     const statement = document.createElement('p')
-     statement.innerText = `This statement has ${total} vowels`
-     statement.classList.add('vowelstatement')
-     bottomHTML.appendChild(statement)
+    // Alert to show how many vowels are present
+    alert(`This statement has ${total} vowels`)
 
-
+    // Clears text input
+    string.value = ""
 })
-
-
-// Functions
-/*
-function getVowels(str) {
-    str = string.value 
-    let total = 0
-
-    return str.replace(/[^aeiou]/gi, "").length
-
-
-    // Will create a statment showing how many vowels are in the string
-    const statement = document.createElement('p')
-    statement.innerText = 'This statement has x vowels'
-    statement.classList.add('vowelstatement')
-    bottomHTML.appendChild(statement)
-
-    console.log(getVowels(string.value))
-}
-
-console.log(getVowels(string.value)) */
-
-
-
